60d6d252f923e782a6cb7ec8944965ed2d29fac4
Mitch Rickman <mitchrickman@gmail.com>
2017-09-11 15:15:26 -0700

    Create add or update action

--- multiadd/controllers/MultiAddController.php
+++ multiadd/controllers/MultiAddController.php
@@ -19,10 +19,10 @@ class MultiAddController extends Commerce_BaseFrontEndController
 
         // Get plugin settings
         $settings = craft()->plugins->getPlugin('multiAdd')->getSettings();
-        
+
         // Settings to control behavour when testing - we don't want to debug via ajax or it stuffs up the JSON response...
         $debug = ($settings->debugPOST and !$ajax);
-      
+
         // Store items added to the cart in case of later failure & rollback required
         $rollback = array();
 
@@ -46,7 +46,7 @@ class MultiAddController extends Commerce_BaseFrontEndController
         } else {
             $itemsToProcess = false;
 
-            // Prevent submission of all 0 qtys            
+            // Prevent submission of all 0 qtys
             foreach ($items as $key => $item) {
                 $qty = isset($item['qty']) ? (int)$item['qty'] : 0;
 
@@ -56,7 +56,7 @@ class MultiAddController extends Commerce_BaseFrontEndController
                 }
             }
 
-            if (!$itemsToProcess) {                
+            if (!$itemsToProcess) {
                 $errors[] = "All items have 0 quantity.";
             }
         }
@@ -66,22 +66,22 @@ class MultiAddController extends Commerce_BaseFrontEndController
             $error = "";
 
             if (!craft()->multiAdd_cart->multiAddToCart($cart, $items, $error)) {
-                $errors[] = $error;  
-            }              
+                $errors[] = $error;
+            }
         }
 
         if ($errors) {
             // Try to log referrer in case of misadventure - might help track down odd errors
             MultiAddPlugin::logError(craft()->request->getUrlReferrer());
 
-            foreach ($errors as $error) {                            
+            foreach ($errors as $error) {
                 MultiAddPlugin::logError($error);
             }
 
             craft()->urlManager->setRouteVariables(['error' => $errors]);
         } else {
             craft()->userSession->setFlash('notice', 'Products have been added to cart');
-            
+
             // Only redirect if we're not debugging and we haven't submitted by ajax
             if (!$debug and !$ajax) {
                 $this->redirectToPostedUrl();
@@ -142,11 +142,94 @@ class MultiAddController extends Commerce_BaseFrontEndController
             craft()->userSession->setError(Craft::t('Couldn’t update line item: {message}', [
                 'message' => $error
             ]));
-            
+
             MultiAddPlugin::logError('Couldn’t update line item: [$error]');
         } else {
             craft()->userSession->setNotice(Craft::t('Items updated.'));
             $this->redirectToPostedUrl();
         }
+    }
+
+    /**
+     * Either update a provided line item or add a new Product to the cart
+     */
+    public function actionAddOrUpdate()
+    {
+      $this->requirePostRequest();
+
+      $ajax = craft()->request->isAjaxRequest();
+      $settings = craft()->plugins->getPlugin('multiAdd')->getSettings();
+      $cart = craft()->commerce_cart->getCart();
+
+      $debug = ($settings->debugPOST and !$ajax);
+      $errors = array();
+
+      // incoming items
+      $items = craft()->request->getPost('items');
+
+      $newItems = [];
+
+      foreach ($items as $item)
+      {
+          $item['qty'] = array_key_exists('qty', $item) ? $item['qty'] : 0;
+
+          if ( array_key_exists('lineItemId', $item) ) {
+
+            $lineItem = craft()->commerce_lineItems->getLineItemById($item['lineItemId']);
+
+            if (!$lineItem || !$lineItem->id || ($cart->id != $lineItem->orderId)) {
+                return true;
+            }
+
+            if ( $lineItem->qty != $item['qty'] ) {
+              // update quantity
+              $lineItem->qty = $item['qty'];
+
+              // save updated line item
+              if (!craft()->commerce_lineItems->updateLineItem($cart, $lineItem, $error)) {
+                $errors[] = $error;
+              }
+            }
+          }
+
+          // verify valid new items - added later
+          else if ( array_key_exists('purchasableId', $item) && $item['qty'] != 0 ) {
+              $newItems[] = $item;
+          }
+      }
+
+      if (!$errors && $newItems) {
+          $error = "";
+
+          if (!craft()->multiAdd_cart->multiAddToCart($cart, $newItems, $error)) {
+              $errors[] = $error;
+          }
+      }
+
+      if ($errors) {
+          // Try to log referrer in case of misadventure - might help track down odd errors
+          MultiAddPlugin::logError(craft()->request->getUrlReferrer());
+
+          foreach ($errors as $error) {
+              MultiAddPlugin::logError($error);
+          }
+
+          craft()->urlManager->setRouteVariables(['error' => $errors]);
+      } else {
+          craft()->userSession->setFlash('notice', 'Your cart has been updated');
+
+          // Only redirect if we're not debugging and we haven't submitted by ajax
+          if (!$debug and !$ajax) {
+              $this->redirectToPostedUrl();
+          }
+      }
+
+      if ( $ajax ) {
+        if ($errors) {
+          $this->returnErrorJson($errors);
+        } else {
+          $this->returnJson(['success' => true, 'cart' => $this->cartArray($cart)]);
+        }
+      }
     }
 }
